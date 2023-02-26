9ee4943bf454e205bb95584cc0abf28d08753bb2
None <banokina03@inbox.ru>
2023-02-26 23:10:13 +0300

    Pr2-3

---
+++ Pr2-3/Pr2-3/Figure.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <iostream>
+#include "Figure.h"
+using namespace std;
+
+void Figure::figure(float x1, float x2, float x3, float x4, float y1, float y2, float y3, float y4)
+{
+    a = ((sqrt((pow((x1 - x4), 2) + pow((y1 - y4), 2)))));
+    b = ((sqrt((pow((x1 - x2), 2) + pow((y1 - y2), 2)))));
+    c = ((sqrt((pow((x2 - x3), 2) + pow((y2 - y3), 2)))));
+    d = ((sqrt((pow((x3 - x4), 2) + pow((y3 - y4), 2)))));
+    d1 = (pow((x1 - x3), 2) + pow((y1 - y3), 2));
+    d2 = (pow((x2 - x4), 2) + pow((y2 - y4), 2));
+    cos = ((((x3 - x1) * (x4 - x2)) + ((y3 - y1) * (y4 - y2))) / ((sqrt((pow((x1 + x3), 2)) + pow((y1 + y3), 2))) + (sqrt((pow((x2 + x4), 2)) + pow((y2 + y4), 2)))));
+    sin = sqrt((1 - pow((cos), 2)));
+    S = (sqrt(d1 * d2) * sin) / 2;
+    P = a + b + c + d;
+}
+
+void Figure::show()
+{
+    cout << a << "," << b << "," << c << "," << d << endl;
+    cout << P << ", " << S;
+}
+
+bool Figure::is_prug()
+{
+    return ((a == c) && (b == d) && (d1 == d2));
+}
+
+bool Figure::is_square()
+{
+    return ((a == b) && (b == c) && (c == d) && (d1 == d2));
+}
+
+bool Figure::is_romb()
+{
+    return ((a == b) && (b == c) && (c == d));
+}
+
+
+bool Figure::is_in_circle()
+{
+    return(sqrt((d1 * d2)) == (a * c + b * d));
+}
+
+bool Figure::is_out_circle()
+{
+    return (a + b == c + d);
+}
---
+++ Pr2-3/Pr2-3/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "Figure.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Figure mas[8];
+
+    float x1, x2, x3, x4;
+    float y1, y2, y3, y4;
+
+    for (int i = 0; i < 3; i++) {
+
+        cout << "Quadrilateral " << i + 1 << " x1,y1 =  " << endl;
+        cin >> x1 >> y1;
+        cout << "Quadrilateral " << i + 1 << " x2,y2 = " << endl;
+        cin >> x2 >> y2;
+        cout << "Quadrilateral " << i + 1 << " x3,y3 = " << endl;
+        cin >> x3 >> y3;
+        cout << "Quadrilateral " << i + 1 << " x4,y4 = " << endl;
+        cin >> x4 >> y4;
+        mas[i].figure(x1, x2, x3, x4, y1, y2, y3, y4);
+
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Parametr " << 1 << ": ";
+        mas[i].show();
+
+        cout << "It is rectangle? " << mas[i].is_prug() << endl;
+        cout << "It is square? " << mas[i].is_square() << endl;
+        cout << "It is rhomb? " << mas[i].is_romb() << endl;
+        cout << "opisana? " << mas[i].is_out_circle() << endl;
+        cout << "vpisana? " << mas[i].is_in_circle() << endl;
+
+    }
+
+}
