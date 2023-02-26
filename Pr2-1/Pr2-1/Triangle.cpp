e2ebce33932fb1f503dff38d6d7216567774221c
None <banokina03@inbox.ru>
2023-02-26 23:07:48 +0300

    Pr2-1

---
+++ Pr2-1/Pr2-1/Triangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include "Triangle.h"
+using namespace std;
+
+bool Triangle::exst_tr() {
+    return ((a + b > c) && (b + c > a) && (a + c > b));
+}
+void Triangle::set(double a1, double b1, double c1) {
+    a = a1;
+    b = b1;
+    c = c1;
+}
+void Triangle::show() {
+    cout << "1 side " << a << endl;
+    cout << "2 side " << b << endl;
+    cout << "3 side " << c << endl;
+}
+double Triangle::perimetr() {
+    double p = a + b + c;
+    return p;
+}
+double Triangle::square() {
+    double p = (a + b + c) / 2;
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    return s;
+}
---
+++ Pr2-1/Pr2-1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include "Triangle.h"
+using namespace std;
+
+
+int main()
+{
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        cout << "Triangle " << i + 1 << " a,b,c = " << endl;
+        cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << " " << endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        setlocale(LC_ALL, "");
+        cout << "Perimetr = " << mas[i].perimetr() << endl;
+        cout << "Square = " << mas[i].square() << endl;
+    }
+    return 0;
+}
